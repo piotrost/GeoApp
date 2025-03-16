d9030f8cacfb38dd251efc47fc571bd03b1c8944
piotrost <piotrostaszewski412@gmail.com>
2025-03-16 13:42:54 +0100

    nic waznego

--- app/src/main/java/pl/pw/geoapp/MainActivity.kt
+++ app/src/main/java/pl/pw/geoapp/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
